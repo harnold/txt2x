aaa28b88de0cdbd43d322f6836a45728dbb88f14
Holger Arnold <git@harnold.org>
2013-02-22 15:53:57 +0100

    Use real umlauts in comments
    LaTeX can read UTF-8 nowadays.

--- scanner.pl
+++ scanner.pl
@@ -1,21 +1,21 @@
 /*
 
 \Predicate scanner/2(+Filename, +Sym_List).
-Hauptpr"adikat. ASCII-File lesen, mit Grammatik Symbole erzeugen,
-Symbole zur"uckgeben
+Hauptprädikat. ASCII-File lesen, mit Grammatik Symbole erzeugen,
+Symbole zurückgeben
 
 \PL*/
 
 scanner(Filename, Sym_List) :-
-    scanner_read_file(Filename, Char_List),    
+    scanner_read_file(Filename, Char_List),
     phrase(scanner_doc(Sym_List), Char_List, _).
 
 /*PL
 
 \Predicate scanner_read_file/2(+Filename, -Char_List).
 
-Dieses Pr"adikat wird f"ur die Dateiarbeit eingesetzt.
-Eine ASCII-Datei wird ge"offnet, die Funktion \verb|scanner_read_char/3|
+Dieses Prädikat wird für die Dateiarbeit eingesetzt.
+Eine ASCII-Datei wird geöffnet, die Funktion \verb|scanner_read_char/3|
 wird zum einlesen der einzelnen Zeichen aufgerufen,
 anschliessend wird die Datei geschlossen.
 
@@ -32,11 +32,11 @@ scanner_read_file(Filename, Char_List) :-
 
 \Predicate scanner_read_char/3(+S, +Chars_In, -Chars_Out).
 
-Aus dem Stream \verb|S| wird die ASCII-Datei Zeichen f"ur Zeichen eingelesen.
+Aus dem Stream \verb|S| wird die ASCII-Datei Zeichen für Zeichen eingelesen.
 Mit jedem Aufruf von \verb|scanner_read_char/3| wird die Anzahl der noch
 zu lesenden Zeichen um genau 1 kleiner, die Liste der gelesenen Zeichen
-wird um 1 l"anger. Bei jedem Aufruf von \verb|dl_push_back| wird zur
-Differenzliste das gerade gelesene Zeichen hinzugef"ugt.
+wird um 1 länger. Bei jedem Aufruf von \verb|dl_push_back| wird zur
+Differenzliste das gerade gelesene Zeichen hinzugefügt.
 
 \verb|scanner_read_char/3| ruft sich solange rekursiv auf, bis das Ende
 der ASCII-Datei erreicht wurde, und ein \verb|end_of_file| gelesen wurde.
@@ -57,7 +57,7 @@ scanner_read_char(S, Chars_In, Chars_Out) :-
 
 \Predicate scanner_write_sym_file/2(+Filename, +Sym_List).
 
-Dieses Pr"adikat gibt die Liste der erzeugten Symbole in
+Dieses Prädikat gibt die Liste der erzeugten Symbole in
 eine Datei mit dem Namen \verb|Filename| aus.
 
 \PL*/
@@ -82,14 +82,14 @@ scanner_doc(Sym_List) -->
 
 \Predicate scanner_doc_rep/2(+In, -Out).
 
-Pr"adikat zum Bearbeiten des Dokumentes.
+Prädikat zum Bearbeiten des Dokumentes.
 
 Es wird \verb|scanner_doc_child| aufgerufen. \verb|scanner_doc_rep| ruft
 sich selbst immer wieder auf, bis \verb|In| leer ist, d.h. keine Zeichen
 mehr zu bearbeiten sind.
 
 Scheitert \verb|scanner_doc_child|, d.h. kein Symbol in der gegebenen
-Reihenfolge mehr lesbar, so wird \verb|scanner_doc_child| beim n"achsten
+Reihenfolge mehr lesbar, so wird \verb|scanner_doc_child| beim nächsten
 Aufruf von \verb|scanner_doc_rep| erneut aufgerufen.
 
 \PL*/
@@ -106,22 +106,22 @@ scanner_doc_rep(In, Out) -->
 \Predicate scanner_doc_child/2(+In, -Out).
 
 Der aktuell zu bearbeitende Textteil wird nach Symbolen durchsucht.
-Es wird versucht Symbol f"ur Symbol in der vorgegebenen Reihenfolge
+Es wird versucht Symbol für Symbol in der vorgegebenen Reihenfolge
 einzulesen. Ist im aktuellen Aufruf kein Lesen eines weiteren Symbols
-mehr m"oglich, wird der verbleibende Resttext beim n"achsten Aufruf
+mehr möglich, wird der verbleibende Resttext beim nächsten Aufruf
 von \verb|scanner_doc_child| gelesen.
 
 In folgender Reihenfolge wird versucht ein Symbol zu lesen:
 
 \begin{enumerate}
-    \item f"uhrende Leerzeichen
+    \item führende Leerzeichen
     \item Leerzeile
     \item vorformatierter Text
     \item Stern-Unterstreichung
     \item doppelte Unterstreichung
     \item einfache Unterstreichung
     \item Listenanstrich
-    \item Aufz"ahlungsanstrich
+    \item Aufzählungsanstrich
     \item einzubettende Objekte (Bilder)
     \item Spaltentrenner
     \item Text
@@ -148,7 +148,7 @@ scanner_doc_child(In, Out) -->
 
 \Predicate scanner_indent/2(+In, -Out).
 
-Dieses Pr"adikat versucht, Whitespaces am Anfang einer Zeile zu lesen,
+Dieses Prädikat versucht, Whitespaces am Anfang einer Zeile zu lesen,
 und erzeugt, falls es diese findet, das Symbol \verb|sym_ident|.
 
 \PL*/
@@ -163,11 +163,11 @@ scanner_indent(In, Out) -->
 
 \Predicate scanner_pre/2(+In, -Out).
 
-Dieses Pr"adikat liest vorformatierten Text.
+Dieses Prädikat liest vorformatierten Text.
 
 Aus \verb|"""T"""| werden die Symbole \verb|sym_pre|, \verb|sym_text(T)|,
-\verb|sym_pre| erzeugt. Alle gefundenen Zeichen werden als Text 1:1 "ubernommen.
-Dazu wird das Pr"adikat \verb|scanner_pretext| aufgerufen.
+\verb|sym_pre| erzeugt. Alle gefundenen Zeichen werden als Text 1:1 übernommen.
+Dazu wird das Prädikat \verb|scanner_pretext| aufgerufen.
 
 \PL*/
 
@@ -183,7 +183,7 @@ scanner_pre(In, Out) -->
 
 \Predicate scanner_pretext/1(-T).
 
-Dieses Pr"adikat liest alle Zeichen bis zum auftreten von \verb|"""|.
+Dieses Prädikat liest alle Zeichen bis zum auftreten von \verb|"""|.
 Wird von \verb|scanner_pre| aufgerufen.
 
 \PL*/
@@ -211,7 +211,7 @@ scanner_presym -->
 
 \Predicate scanner_text/2(+In, -Out).
 
-Dieses Pr"adikat liest Text, und erzeugt das Symbol \verb|sym_text(T)|.
+Dieses Prädikat liest Text, und erzeugt das Symbol \verb|sym_text(T)|.
 Das Lesen der einzelnen Zeichen wird beendet, wenn ein Zeilenende oder
 ein Spaltentrenner gefunden wurde.
 
@@ -226,7 +226,7 @@ scanner_text(In, Out) -->
 /*PL
 
 \Predicate scanner_break/2(+In, -Out).
-Dieses Pr"adikat findet eine Leerzeile, und erzeugt dann das Symbol
+Dieses Prädikat findet eine Leerzeile, und erzeugt dann das Symbol
 \verb|sym_break|.
 
 \PL*/
@@ -242,9 +242,9 @@ scanner_break(In, Out) -->
 
 \Predicate scanner_colseperator/2(+In, -Out).
 
-Dieses Pr"adikat findet Spaltentrenner, und erzeugt dann das Symbol
+Dieses Prädikat findet Spaltentrenner, und erzeugt dann das Symbol
 \verb|sym_col_seperator|. Whitespaces zwischen dem Spaltentrenner und dem
-nachfolgenden Text werden "uberlesen.
+nachfolgenden Text werden überlesen.
 
 \PL*/
 
@@ -259,16 +259,16 @@ scanner_colseparator(In, Out) -->
 
 \Predicate scanner_cdata/1(-T).
 
-Dieses Pr"adikat liest Zeichen fuer \verb|sym_text|. Das Einlesen wird beim
+Dieses Prädikat liest Zeichen fuer \verb|sym_text|. Das Einlesen wird beim
 Auftreten von Newline oder dem Spaltentrennsymbol beendet.
 
 \PL*/
 
 scanner_cdata(T) -->    [ T1 ],
-    {        
+    {
         ( T1 = '\n'  -> fail; true ),
         ( T1 = '|'  -> fail; true )
-    }, 
+    },
     scanner_cdata(T2), !,
     {
         string_concat(T1, T2, T)
@@ -284,7 +284,7 @@ scanner_cdata(T) -->
 
 \Predicate scanner_white/0().
 
-Dieses Pr"adikat liest Zeichen, solange diese Whitespaces sind.
+Dieses Prädikat liest Zeichen, solange diese Whitespaces sind.
 
 \PL*/
 
@@ -305,7 +305,7 @@ scanner_white -->
 
 \Predicate scanner_starul/2(+In, -Out).
 
-Dieses Pr"adikat sucht Unterstreichungen mit \verb|*****|. Wird diese Art der
+Dieses Prädikat sucht Unterstreichungen mit \verb|*****|. Wird diese Art der
 Unterstreichung gefunden, so wird das Symbol \verb|sym_star_ul| erzeugt.
 
 \PL*/
@@ -324,7 +324,7 @@ scanner_starulsym -->
 
 \Predicate scanner_doubleul/2(+In, -Out).
 
-Dieses Pr"adikat sucht Unterstreichungen mit \verb|=====|. Wird diese Art der
+Dieses Prädikat sucht Unterstreichungen mit \verb|=====|. Wird diese Art der
 Unterstreichung gefunden, so wird das Symbol \verb|sym_double_ul| erzeugt.
 
 \PL*/
@@ -343,7 +343,7 @@ scanner_doubleulsym -->
 
 \Predicate scanner_singleul/2(+In, -Out).
 
-Dieses Pr"adikat sucht Unterstreichungen mit \verb|-----|. Wird diese Art der
+Dieses Prädikat sucht Unterstreichungen mit \verb|-----|. Wird diese Art der
 Unterstreichung gefunden, so wird das Symbol \verb|sym_single_ul| erzeugt.
 
 \PL*/
@@ -362,11 +362,11 @@ scanner_singleulsym -->
 
 \Predicate scanner_include/2(+In, -Out).
 
-Dieses Pr"adikat sucht einzubettende Objekte, zum Beipiel Bilder. Werden die
+Dieses Prädikat sucht einzubettende Objekte, zum Beipiel Bilder. Werden die
 Zeichen \verb|<<| und \verb|>>| gefunden, so werden die Zeichen dazwischen
-als Text "ubergeben.
+als Text übergeben.
 
-Von diesem Pr"adikat werden folgende Symbole erzeugt: \verb|sym_begin_include|,
+Von diesem Prädikat werden folgende Symbole erzeugt: \verb|sym_begin_include|,
 \verb|sym_text(T)| und \verb|sym_end_include|. Es wird
 \verb|scanner_cd| aufgerufen.
 
@@ -384,15 +384,15 @@ scanner_include(In, Out) -->
 
 \Predicate scanner_cd/1(T).
 
-Dieses Pr"adikat wird von \verb|scanner_include| aufgerufen.
+Dieses Prädikat wird von \verb|scanner_include| aufgerufen.
 Der Text zwischen \verb|<<| und \verb|>>| wird eingelesen.
 
 \PL*/
 
 scanner_cd(T) -->    [ T1 ],
-    {        
+    {
         ( T1 = '>'  -> fail; true )
-    }, 
+    },
     scanner_cd(T2), !,
     {
         string_concat(T1, T2, T)
@@ -415,9 +415,9 @@ scanner_end_include_sym -->
 
 \Predicate scanner_list_bullet/2(+In, -Out).
 
-Dieses Pr"adikat findet Listenanstriche, und erzeugt dann das Symbol
+Dieses Prädikat findet Listenanstriche, und erzeugt dann das Symbol
 \verb|sym_list_bullet|. Whitespaces zwischen dem Listenanstrich und dem
-nachfolgenden Text werden "uberlesen.
+nachfolgenden Text werden überlesen.
 
 \PL*/
 
@@ -432,7 +432,7 @@ scanner_list_bullet(In, Out) -->
 
 \Predicate scanner_enum_bullet/2(+In, -Out).
 
-Dieses Pr"adikat findet Aufzaehlungsanstriche, und erzeugt dann das Symbol
+Dieses Prädikat findet Aufzaehlungsanstriche, und erzeugt dann das Symbol
 \verb|sym_enum_bullet|. Whitespaces zwischen dem Aufzaehlungsanstrich und
 dem nachfolgenden Text werden ueberlesen.
 
@@ -449,7 +449,7 @@ scanner_enum_bullet(In, Out) -->
 
 \Predicate scanner_white_opt/2(+In, -Out).
 
-Dieses Pr"adikat "uberliest 0..n Whitespaces. Es wird aufgerufen von
+Dieses Prädikat überliest 0..n Whitespaces. Es wird aufgerufen von
 \verb|scanner_list_bullet|, \verb|scanner_enum_bullet| und
 \verb|scanner_colseperator|.
 
